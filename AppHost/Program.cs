2aefa68881c9905aeed30364249dbcdd1ad61f66
nandotmbn <orlandopratama695@gmail.com>
2025-06-18 16:56:48 +0700

    feat: init event driven

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -4,4 +4,6 @@ builder.AddProject<Projects.WebAPI>("webapi");
 
 builder.AddProject<Projects.GraphQL>("graphql");
 
+builder.AddProject<Projects.EventDriven>("eventdriven");
+
 builder.Build().Run();
